926b20f767ff5bc9712cdc03be1587cd2c8f386c
Zevhys <rakha2728@gmail.com>
2024-11-01 22:52:43 +0800

    update schema

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, "username is required"],
+    unique: [true, "username unique is required"],
   },
   password: {
     type: String,
